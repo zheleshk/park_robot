737ac41ce6cd42b1ec93d8ecf62f0fb5e5ad010f
zheleshk <kirillch030309@gmail.com>
2025-10-24 14:31:14 +0300

    before project protection

--- src/robot_bringup/launch/headless.launch.py
+++ src/robot_bringup/launch/headless.launch.py
@@ -4,23 +4,32 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    urdf_file_path = os.path.join(
+    # Путь к нашему минимальному URDF
+    urdf_file = os.path.join(
         get_package_share_directory('robot_description'),
         'urdf',
         'park_robot.urdf'
     )
 
-    with open(urdf_file_path, 'r') as f:
+    # Читаем содержимое URDF
+    with open(urdf_file, 'r') as f:
         robot_description_content = f.read()
 
+    # Нода, которая публикует описание робота
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
         parameters=[{'robot_description': robot_description_content}]
     )
 
+    # Нода RViz
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2'
+    )
+
     return LaunchDescription([
-        robot_state_publisher_node
+        robot_state_publisher_node,
+        rviz_node
     ])
